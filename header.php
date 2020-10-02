1575d739ff5109e9d045e292d453c78d3599099f
Sergio <sergioralandison@outlook.com>
2020-10-02 14:29:30 +0200

    work in progress

--- header.php
+++ header.php
@@ -8,5 +8,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-    <?php wp_body_open(); ?>
+  <header class="header">
+    <a href="<?php echo home_url( '/' ); ?>">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+    </a>
+  </header>
+  <?php wp_body_open(); ?>
